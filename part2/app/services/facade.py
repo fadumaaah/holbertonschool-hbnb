a605c86bae586843df4a3ab50928ca067f84233b
fadumaaah <10507@holbertonstudents.com>
2025-06-20 06:15:54 +1000

    implent amenity endpoints

--- part2/app/services/facade.py
+++ part2/app/services/facade.py
@@ -1,4 +1,4 @@
- from app.persistence.repository import InMemoryRepository
+from app.persistence.repository import InMemoryRepository
 
 class HBnBFacade:
     def __init__(self):
@@ -16,3 +16,20 @@ class HBnBFacade:
     def get_place(self, place_id):
         # Logic will be implemented in later tasks
         pass
+
+    def create_amenity(self, amenity_data):
+    # Placeholder for logic to create an amenity
+        pass
+
+    def get_amenity(self, amenity_id):
+    # Placeholder for logic to retrieve an amenity by ID
+        pass
+
+    def get_all_amenities(self):
+    # Placeholder for logic to retrieve all amenities
+        pass
+
+    def update_amenity(self, amenity_id, amenity_data):
+    # Placeholder for logic to update an amenity
+        pass
+    
